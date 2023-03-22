5588514113805a6353d6af452f3edf0d29c24cec
Supakxrn <126548616+Supakxrn@users.noreply.github.com>
2023-03-22 15:23:29 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -13,7 +13,7 @@ def test_get_call_name_jonancy():
     name = "Jonancy"
     url = "/callname/{name}"
     response = client.get(url)
-    expected_result = {"hello": name}
+    expected_result = {"hello": "Jonancy"}
     assert response.status_code == 200
     assert response.json() == expected_result
 
@@ -21,6 +21,6 @@ def test_post_call_name_paipibut():
     name = "Paipibut"
     url = "/callname"
     response = client.post(url)
-    expected_result = {"hello": name}
+    expected_result = {"hello": "Paipibut"}
     assert response.status_code == 200
     assert response.json() == expected_result
